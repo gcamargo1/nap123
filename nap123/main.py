a2b848a4ce62809cb3860825c44600185224102f
Gustavo Camargo <gustavo.camargo@bayer.com>
2020-10-22 12:35:11 -0500

    first function

---
+++ nap123/main.py
@@ -0,0 +1,11 @@
+def say_hello(name: str = "Gustavo") -> None:
+    """
+    Prints a name of a person
+
+    Args:
+        name: The name of the person
+
+    Returns: Nothing
+
+    """
+    print(f"hello {name}")
